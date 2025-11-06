ccccf196c50fcf31236e7e31ac65ece7df44f28e
Aditya019 <adityapande019@gmail.com>
2025-11-06 11:37:58 +0530

    Variable Understanding

---
+++ 01Basic/01Variable.js
@@ -0,0 +1,7 @@
+const accontId=1234
+let accountName="Aditya"
+var accountEmail="aditya@gmail.com"
+accountCity="Jaipur"
+let accountState;
+
+console.table([accontId,accountEmail,accountName,accountState,accountCity])
